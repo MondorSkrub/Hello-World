14ba0a85d89166f8ec7387668d62b60ac977441a
Colwin Mondor <31044181+MondorSkrub@users.noreply.github.com>
2017-08-15 14:07:42 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,37 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+				
+		       
+	    int x = 6;
+	    int y = 10;
+	    
+	    
+	    {	    
+	    {
+	     int n = 15;
+	      int ans = 1;
+	    	    	
+	    	for(int i=1; i <=n; i=i+1)
+	    	{
+	    		ans = i*ans;
+	    		
+	    	}
+	    	System.out.println(ans);
+	    
+	    	
+	    int m = 5;
+	    if(m == 0);
+	    {
+	    	System.out.println(Undefined);
+	    }
+	    
+	    {
+	    	System.out.println((double)1/m);
+	    }
+	    }
+	}
+	}
+}
